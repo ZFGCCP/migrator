6b0f24812a9ec5aca07d609170d7c11fa3b2a6ff
MGZero <onisephiroth@gmail.com>
2025-02-09 22:12:43 -0500

    initial commit

---
+++ src/main/java/com/zfgc/zfgbb/migrator/config/DatasourceConfig.java
@@ -0,0 +1,25 @@
+package com.zfgc.zfgbb.migrator.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DatasourceConfig {
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix="spring.datasource")
+	public DataSource primaryDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix="zfgbb.mysqldatasource")
+	public DataSource secondaryDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
+}
