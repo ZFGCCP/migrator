ebfda43e6b728f03b18f47707c3e1c3c39177adc
steve <onisephiroth@gmail.com>
2025-02-10 20:20:42 -0500

    convert boards, categories and users

--- src/main/java/com/zfgc/zfgbb/migrator/MigratorApplication.java
+++ src/main/java/com/zfgc/zfgbb/migrator/MigratorApplication.java
@@ -1,5 +1,6 @@
 package com.zfgc.zfgbb.migrator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/zfgc/zfgbb/migrator/config/DatasourceConfig.java
+++ src/main/java/com/zfgc/zfgbb/migrator/config/DatasourceConfig.java
@@ -2,24 +2,63 @@ package com.zfgc.zfgbb.migrator.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
+@MapperScan(basePackages="com.zfgc.zfgbb.migrator.mappers",
+			sqlSessionTemplateRef="sqlSessionTemplatePostgres")
+@MapperScan(basePackages="com.zfgc.zfgbb.migrator.smf.mappers",
+			sqlSessionTemplateRef="sqlSessionTemplateMySql")
 public class DatasourceConfig {
-	@Bean
+	@Bean(name="postgresds")
 	@Primary
 	@ConfigurationProperties(prefix="spring.datasource")
-	public DataSource primaryDataSource() {
+	public DataSource postgresDataSource() {
 	    return DataSourceBuilder.create().build();
 	}
 
-	@Bean
+	@Bean(name="smfds")
 	@ConfigurationProperties(prefix="zfgbb.mysqldatasource")
-	public DataSource secondaryDataSource() {
+	public DataSource mysqlDataSource() {
 	    return DataSourceBuilder.create().build();
 	}
+	
+	@Bean
+    public SqlSessionFactory sqlSessionFactoryPostgres() throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(postgresDataSource());
+        //Set the storage location of XML files Note the student directory here
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources("classpath:com/zfgc/zfgbb/migrator/mappers/*.xml")); 
+        return bean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplatePostgres() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactoryPostgres());
+    }
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryMysql() throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(mysqlDataSource());
+        //Set the storage location of XML files Note the student directory here
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources("classpath:com/zfgc/zfgbb/migrator/smf/mappers/*.xml")); 
+        return bean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplateMySql() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactoryMysql());
+    }
 }
